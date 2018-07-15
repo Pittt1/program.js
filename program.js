8359b303ed1a324f7420a2bee2a12517230a0064
Piotr Slawek <piotr.slawek@o2.pl>
2018-07-15 22:58:03 +0200

    Change content in program.js file

--- program.js
+++ program.js
@@ -4,19 +4,19 @@ process.stdin.on('readable', function() {
 	var input = process.stdin.read();
 	if (input !== null) {
 		var instruction = input.toString().trim();
-			switch (instruction) {
-				case 'version':
-					console.log(process.versions);
-					break;
-				case 'environment':
-					console.log(process.env);
-					break;
-				case '/exit':
-					process.stdout.write('Quitting app!\n');
-					process.exit();
-					break;
-				default:
-					process.stdout.write('Wrong instruction!\n');
-			}
+		switch (instruction) {
+			case 'version':
+				console.log(process.versions);
+				break;
+			case 'environment':
+				console.log(process.env);
+				break;
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			default:
+				process.stdout.write('Wrong instruction!\n');
+		}
 	}
 });
